54795b39f8f03f1483deb987f82965d38ce8098c
Lezurex <lenny.angst@easyid.ch>
2021-02-03 11:54:53 +0100

    Start data loader Add data.json repairer

--- Timestamp.php
+++ Timestamp.php
@@ -2,6 +2,22 @@
 
 namespace Zeiterfassung;
 
+use DateTime;
+
 class Timestamp {
+
+    private DateTime $start;
+    private DateTime $end;
+    private string $project;
+
+    /**
+     * Timestamp constructor.
+     * @param DateTime $start
+     * @param string $project
+     */
+    public function __construct(DateTime $start, string $project) {
+        $this->start = $start;
+        $this->project = $project;
+    }
 
 }
--- timesheet.php
+++ timesheet.php
@@ -5,6 +5,7 @@ use Zeiterfassung\Person;
 
 require_once 'Timestamp.php';
 require_once 'Person.php';
+
 /**
  * Main class for the time tracking application
  */
@@ -13,6 +14,11 @@ class App {
     public array $persons;
     public array $timestamps;
 
+    private static array $DEFAULT_DATA_STRUCTURE = array(
+        "persons" => array(),
+        "timestamps" => array()
+    );
+
     private Person $currentPerson;
 
     /**
@@ -20,6 +26,9 @@ class App {
      */
     public function __construct() {
         $this->persons = array();
+        $this->timestamps = array();
+
+        $this->loadData();
 
         print "Welcome to the time tracking application!";
         $validInput = false;
@@ -32,8 +41,37 @@ class App {
                 $this->persons[] = $this->currentPerson;
                 $validInput = true;
             }
-        } while(!$validInput);
+        } while (!$validInput);
         print "\nWelcome, {$this->currentPerson->getFullName()}";
+    }
+
+    private function loadData() {
+        $json = null;
+        if (false == ($json = @file_get_contents("data.json"))) {
+            file_put_contents("data.json", json_encode(self::$DEFAULT_DATA_STRUCTURE));
+        } else {
+            $data = json_decode($json, true);
+            if (!is_null($data) && isset($data['timestamps']) && isset($data['persons'])) {
+                foreach ($data['timestamps'] as $timestampData) {
+
+                }
+            } else {
+                $this->regenerationSequence();
+            }
+        }
+    }
+
+    private function regenerationSequence() {
+        print "The data file is corrupt or not readable. Do you want to regenerate it? This will result in data loss! (y/n) ";
+        $input = readline();
+        if (strtoupper($input) == "Y") {
+            file_put_contents("data.json", json_encode(self::$DEFAULT_DATA_STRUCTURE));
+            print "\nData file regenerated!";
+            $this->loadData();
+            return;
+        }
+        print "\nTry repairing data.json by yourself, then restart. Exiting now.";
+        exit();
     }
 
 
